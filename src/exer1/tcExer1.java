6d927dc47f30bc6ef5f081b036705c9f7418c9c7
jmesteve <jestevesoler@gmail.com>
2013-04-30 23:15:38 +0200

    Test falla por numero incorrecto de lineas en el output

--- src/exer1/tcExer1.java
+++ src/exer1/tcExer1.java
@@ -1,34 +1,39 @@
 package exer1;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.util.Scanner;
 
 public class tcExer1 {
 
-/*	
-2
-2
-1 2 10 4 10 1
-5
-3 4 6 7 8 2 4 5
-*/
+public static void main(String[] args) throws AWTException {
 
-public static void main(String[] args) {
-
+	Robot robot = new Robot();
+	
 	Scanner sc = new Scanner(System.in);
 	int v_casos = sc.nextInt();
 	int v_budget = 0;
 	String[] v_precios;
+	String v_text = "";
 
-
-
-	for (int i = 0; i < v_casos; i++) {
+	
+	for (int j = 0; j < v_casos; j++) {
+				
 		v_budget = sc.nextInt();
 		sc.nextLine();
-		String v_text = sc.nextLine();
+		v_text = sc.nextLine();
 		v_precios = v_text.split(" ");
-		System.out.println("BUDGET " + v_budget + " TEXTO " + v_text + "PASTA" + howMuchpasta(v_precios, v_budget));
+		System.out.println(howMuchpasta(v_precios, v_budget));
+		
+		if (j == 8) {
+			
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			
+		}
+		
 	}	
-
 	
 }
 
@@ -38,38 +43,32 @@ public static int howMuchpasta(String[] p_precios, int p_budget) {
 	int v_BC = 0;
 
 	for (int i = 0; i < p_precios.length; i++) {
-
-
-		System.out.println("PRECIO " + p_precios[i] + " PASTA " + v_money + " BC " + v_BC);
-
-		if (v_BC == 0) { //Si no tengo BC comprar
-			 //Si precio <= p_budget && precio < sig. && preciopos no ultimo
+	
+			//Buy i you have money enought, next price is higher and you have time to sell
 			if (Integer.parseInt(p_precios[i]) <= v_money) {
 				if ((i+1)< p_precios.length) {
 					if (Integer.parseInt(p_precios[i+1]) > Integer.parseInt(p_precios[i])) {
 						v_BC = (int)v_money/Integer.parseInt(p_precios[i]); 
-						/*OJO solo enteros? puedo comprar algunos y quedarme pasta?*/
-						/*MAS OJO PUEDO COMPRAR MAS BC mientras TENGO BC!!*/						
 						v_money = v_money - v_BC*Integer.parseInt(p_precios[i]);
-
-						System.out.println("COMPRO " + v_BC + " A " + p_precios[i]);
 					}					
 				}
 			}
-		} else { //Si tengo BC tengo que vender
-			//Mientras el precio siguiente sea mayor y no sea el ultimo no se vende
-			if ((i+1) >= p_precios.length) {
-				v_money = v_money +(v_BC * Integer.parseInt(p_precios[i]));
+		
+			//Sell if you have something to
+			if (v_BC > 0) {
+				
+				//Mientras el precio siguiente sea mayor y no sea el ultimo no se vende
+				//While the next price still higher and not last we dont sell
+				if ((i+1) >= p_precios.length) {
+					v_money = v_money +(v_BC * Integer.parseInt(p_precios[i]));
+					v_BC = 0;
 
-				System.out.println("1 VENDO " + v_BC + " A " + p_precios[i]);
-				v_BC = 0;
-
-			} else if (Integer.parseInt(p_precios[i+1])<Integer.parseInt(p_precios[i])) {
-				v_money = v_money + (v_BC * Integer.parseInt(p_precios[i]));
-				System.out.println("2 VENDO " + v_BC + " A " + p_precios[i]);
-				v_BC = 0;
+				} else if (Integer.parseInt(p_precios[i+1])<Integer.parseInt(p_precios[i])) {
+					v_money = v_money + (v_BC * Integer.parseInt(p_precios[i]));
+					v_BC = 0;
+				}
 			}
-		}
+		
 	}
 	
 	return v_money;
